56b8daef436db0238550de1a6890c27f6f7b5986
camilojimenezf <camilob270@gmail.com>
2025-11-20 13:21:47 -0300

    remove hardcoded userId

--- src/modules/notification/actions/get-notifications-history.action.ts
+++ src/modules/notification/actions/get-notifications-history.action.ts
@@ -4,7 +4,7 @@ import type { NotificationHistoryResponse } from "../interfaces/notification-his
 export const getNotificationsAction =
   async (): Promise<NotificationHistoryResponse> => {
     const { data } = await notificationApi.get<NotificationHistoryResponse>(
-      `/notification-events/user/?userId=4af0e87d-d165-4efc-90c3-d73d7046f55e`
+      "/notification-events/user"
     );
     return data;
   };
